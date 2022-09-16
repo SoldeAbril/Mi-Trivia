b6a9ae8b20e3aa4d56ee3048e4ad206b9a2ed44e
SoldeAbril <51126328+SoldeAbril@users.noreply.github.com>
2022-09-16 22:49:52 +0000

    Se agrego pruimera funcionalidad

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-nombre="Preguntas"
-print(nombre)
+# Lo primero es mostrar en pantalla el texto de bienvenida para quien juegue tu trivia
+print ("Bienvenido a mi trivia sobre computación")
+print ("Pondremos a prueba tus conocimientos")
+
+# Es importante dar instrucciones sobre cómo jugar:
+print ("Responder las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Entrer' para enviar tu respuesta:\n")
+
+# OJO, el \n al final de la línea 6 es para dar un "salto de línea"
+
+# Pregunta 1
+print ("1) ¿Quién fue el creador de windows?")
+print ("a) Linus Torvalds")
+print ("b) Bill Gates")
+print ("c) Mark Zuckerberg")
+print ("d) Dennis Ritchie")
+
+# Almacenamos la respuesta del usuario en la variable "respuesta_1"
+respuesta_1 = input("\nTu respuesta: ")
+
+
+# Selección de respuesta
+
+if respuesta_1 == "b":
+  print ("¡Correcto!")
+
+else:
+  print ("Intenta de nuevo")
+
